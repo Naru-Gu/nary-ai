809a48316c43898dc7975222f5963f1c2e127fd9
NaryGu <63098570+zeng-bw@users.noreply.github.com>
2023-04-18 11:45:05 +0800

    Add files via upload
    create project

---
+++ src/main/java/top/nary/naryai/NaryAiApplication.java
@@ -0,0 +1,13 @@
+package top.nary.naryai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaryAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaryAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/nary/naryai/NaryAiApplicationTests.java
@@ -0,0 +1,13 @@
+package top.nary.naryai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaryAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
